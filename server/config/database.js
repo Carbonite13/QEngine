a0b8f741d866deacce574d1cb96e2bcece8f3ea7
carbonite13 <107830966+Carbonite13@users.noreply.github.com>
2026-06-18 11:29:41 +0530

    Sequelize migration for v6 to v7

--- server/config/database.js
+++ server/config/database.js
@@ -7,8 +7,9 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432, // Port added as per sequlize v7
         dialect: process.env.DB_DIALECT,
-        logging: console.log // Enable logging
+        logging: console.log,
     },
 );
 
