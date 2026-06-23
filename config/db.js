d6040d6bdfdb6aae859f2e457337269321261a44
Carbonite13 <107830966+Carbonite13@users.noreply.github.com>
2026-06-23 12:26:18 +0530

    Migrate to dotenv,
    added sample env file

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
-// config/db.js
 const { Sequelize } = require("@sequelize/core");
 const { MySqlDialect } = require("@sequelize/mysql");
 
+require('dotenv').config({ path: '.env.local', override: true });
+
 const sequelize = new Sequelize({
     dialect: MySqlDialect,
-    database: "eims_db",
-    user: "eims_admin",
-    password: "pass",
-    host: "localhost",
-    port: 3306,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     logging: false,
 });
 
