1daddd950d417a8b84cddb5a760036d15aa64d96
Carbonite13 <107830966+Carbonite13@users.noreply.github.com>
2026-06-24 18:11:24 +0530

    morgan: Add update tot ignore logs from /openapi.json

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ import sequelize from "./config/db.js";
 import employeeRoutes from "./routes/EmployeeRoutes.js";
 import errorHandler from "./middleware/ErrorHandler.js";
 
+
+
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan("dev", {
+    skip: (req, res) => {
+        return req.path === "/openapi.json";
+    }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
